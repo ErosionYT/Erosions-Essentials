2c48e7234d485bfbb6e1766f778de8eb30823b9d
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2022-02-22 12:02:12 +0000

    Update AnnoucementsTask.php

--- src/ErosionYT/Essentials/Tasks/AnnoucementsTask.php
+++ src/ErosionYT/Essentials/Tasks/AnnoucementsTask.php
@@ -6,32 +6,15 @@ use ErosionYT\Essentials\Main;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
 use pocketmine\plugin;
+use pocketmine\utils\Config;
 use function array_rand;
 
-class AnnoucementsTask extends Task
+class AnnouncementsTask extends Task
 {
-    public $plugin;
-    public $config;
-
-    public $lastMessage = [];
-
-    public function __construct(Main $plugin)
-    {
-        $this->plugin = $plugin;
-        $this->config = $plugin->getConfig();
-    }
-
     public function onRun() : void
     {
-        $online = count(Server::getInstance()->getOnlinePlayers());
-        $plugin = $this->plugin;
         $announcements = (array) Main::getInstance()->getConfig()->get("announcements");
 
-        if (count(Server::getInstance()->getOnlinePlayers()) > 0) {
-            Main::getInstance()->getServer()->broadcastMessage("§a   §b\n§7 » ".$announcements[array_rand($announcements)]."\n§e      §d");
-
-        }
-
+        if (count(Server::getInstance()->getOnlinePlayers()) > 0) Main::getInstance()->getServer()->broadcastMessage("§a   §b\n§7 » ". $announcements[array_rand($announcements)] ."\n§e      §d");
     }
-
 }
