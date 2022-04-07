daadfc059761707a6b3b120e647232a37adfce49
ErosionYT <kcorb05@outlook.com>
2022-04-07 20:25:25 +1000

    Bug fix

--- src/ErosionYT/Essentials/Commands/PingCommand.php
+++ src/ErosionYT/Essentials/Commands/PingCommand.php
@@ -4,6 +4,7 @@ namespace ErosionYT\Essentials\Commands;
 
 use ErosionYT\Essentials\Main;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\command\Command;
@@ -24,6 +25,11 @@ class PingCommand extends Command implements PluginOwned {
     public function execute(CommandSender $sender, string $commandLabel, array $args): void {
         if (!$sender->hasPermission($this->getPermission())) {
             $sender->sendMessage($this->getPermissionMessage());
+            return;
+        }
+
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Use the command in-game");
             return;
         }
 
